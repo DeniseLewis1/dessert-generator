d7df312033390bef15541b71293188169507ca1e
Denise <deniselewis1120@gmail.com>
2023-08-02 15:53:29 -0700

    Created a data file with a template for each dessert object

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const desserts = [
+    {
+        id: 1,
+        name: "",
+        description: "",
+        flavors: [],
+        ingredients: [],
+        image: "",
+        servings: "",
+        skill: "",
+        type: ""
+    }
+];
+
+export default desserts;
+
+/***** Dessert fields *****/
+/*
+    id: unique id
+    name: dessert name
+    description: brief description
+    flavors: list of flavors
+    ingredients: list of ingredients
+    image: image url
+    servings: small (0 - ?), medium (? - ?), large (> ?)
+    skill: easy. medium, hard
+    type: dessert type
+*/
+
+/***** Desserts *****/
+/*
+    1.
+    2.
+    3.
+    ...
+*/
